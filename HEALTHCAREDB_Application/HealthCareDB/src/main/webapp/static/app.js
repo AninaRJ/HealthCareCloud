ffe5f459e2aed86992e87175f8dcc49753524d26
AninaRJ <ranjani.shivashankar@gmail.com>
2016-08-25 19:33:40 +0530

    Uncomment the contextRoot setting

--- HEALTHCAREDB_Application/HealthCareDB/src/main/webapp/static/app.js
+++ HEALTHCAREDB_Application/HealthCareDB/src/main/webapp/static/app.js
@@ -2,10 +2,10 @@
  * http://usejsdoc.org/
  */
 var hccApp = angular.module("hccApp", ['ngMessages', 'ngMaterial', 'googleApi']).run(function($rootScope) {
-	/*if (location.protocol != 'https:') {
+	if (location.protocol != 'https:') {
 		location.protocol = 'https:';
 	}
-	$rootScope.contextRoot = location.protocol + '//' + location.hostname + (location.port ? ':' + location.port: '') + "/HealthCareDB/";*/
+	$rootScope.contextRoot = location.protocol + '//' + location.hostname + (location.port ? ':' + location.port: '') + "/HealthCareDB/";
 }).config(function(googleLoginProvider, $parseProvider, $mdThemingProvider) {
     googleLoginProvider.configure({
         clientId: '1035365942198-a6n1gvb87pdetflppdd1a2qav41lufsd.apps.googleusercontent.com',
